8124435d481521b6d365432eca39191ba335b16d
Seywuhoo <ferdzadsyameel@gmail.com>
2024-10-23 13:25:23 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I have something for you'.split('')
+const text = 'MY LOVE, PLEASE BELIEVE ME WHEN I SAY THIS'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`

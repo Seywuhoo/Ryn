4889a3140ee536deb17b6d49689df8a112b90d23
Seywuhoo <ferdzadsyameel@gmail.com>
2024-10-31 17:11:56 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('AWAK LAH BUNGA TERINDAH SAYA').split('')
+    const titles = ('Awak Bunga Terindah Saya').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 

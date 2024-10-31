a96c77f031dc5a0f29d4e02f7966da4e3fadad1e
Seywuhoo <ferdzadsyameel@gmail.com>
2024-10-31 17:04:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('YOU ARE PERFECT').split('')
+    const titles = ('AWAK LAH BUNGA TERINDAH SAYA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 

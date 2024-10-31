2e58d1ddd65ccc7d92547bd6454af71e07102198
Seywuhoo <ferdzadsyameel@gmail.com>
2024-10-31 17:10:42 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Dalam banyak bunga yang saya petik,'.split('')
+const text = 'Disetiap bunga yang saya petik'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
